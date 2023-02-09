9e4fc57f27b649f7b48fabe68734b9bc02b07c81
Ahmedosama <Ahmedosamacis01@outlook.com>
2023-02-09 02:40:54 +0200

    Add index view model

---
+++ ADHOM Store/Models/indexVm.cs
@@ -0,0 +1,17 @@
+﻿namespace ADHOM_Store.Models
+{
+    public class indexVm
+    {
+
+        public indexVm()
+        {
+            CategoriesVm = new List<Category>();
+            ProductsVm= new List<Product>();  
+            ReviewsVm= new List<Review>();
+        }
+       
+        public List<Category> CategoriesVm { get; set; }
+        public List<Product> ProductsVm { get; set; }
+        public List<Review> ReviewsVm { get; set; }
+    }
+}
