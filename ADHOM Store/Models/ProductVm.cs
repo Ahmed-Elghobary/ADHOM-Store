331e2d6133d00b2ffd4416a1b05d8906c00ff9e3
Ahmedosama <Ahmedosamacis01@outlook.com>
2023-02-12 21:51:18 +0200

    Add View model

---
+++ ADHOM Store/Models/ProductVm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ADHOM_Store.Models
+{
+    public class ProductVm
+    {
+        [Required]
+        [Display(Name ="Category Name")]
+        public string CatName_Vm { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductName_Vm { get; set; }
+        public decimal ProductPrice_Vm { get; set; }
+
+        public int ProductQt_Vm { get; set; }
+    }
+}
--- ADHOM Store/Models/indexVm.cs
+++ ADHOM Store/Models/indexVm.cs
@@ -8,10 +8,12 @@
             CategoriesVm = new List<Category>();
             ProductsVm= new List<Product>();  
             ReviewsVm= new List<Review>();
+            LatestProduct = new List<Product>();
         }
        
         public List<Category> CategoriesVm { get; set; }
         public List<Product> ProductsVm { get; set; }
+        public List<Product> LatestProduct { get; set; }
         public List<Review> ReviewsVm { get; set; }
     }
 }
