8018503e754913afea07867b3d2db83471879d8f
Ahmedosama <Ahmedosamacis01@outlook.com>
2023-02-15 03:21:27 +0200

    Add js file

---
+++ ADHOM Store/wwwroot/assets/js/myScript.js
@@ -0,0 +1,24 @@
+﻿$.ajax({
+    url: "https://localhost:7035/api/Values/GetProducts",
+    type: "get",
+    contentType: "application/json",
+    success: function (result, status, xhr) {
+        console.log(result);
+        $.each(result.$values, function (index, value) {
+            $("tbody").append($("<tr>"));
+            appendElement = $("tbody tr").last();
+            appendElement.append($("<td>").html(value["id"]));
+            appendElement.append($("<td>").html(value["name"]));
+            appendElement.append($("<td>").html(value["price"]));
+            appendElement.append($("<td>").html(value["qty"]));
+            if (value["cat"]) {
+                appendElement.append($("<td>").html(value["cat"]["catId"]));
+            }
+        appendElement.append($("<td>").html(""));
+            
+        });
+    },
+    error: function (xhr, status, error) {
+        console.log(xhr)
+    }
+});
