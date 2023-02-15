c8e1ef5f47d1eaa555591dff7df2cba163cb04bb
Ahmedosama <Ahmedosamacis01@outlook.com>
2023-02-15 03:20:44 +0200

    Add api controllere and view

---
+++ ADHOM Store/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using ADHOM_Store.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ADHOM_Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        AdhomContext db = new AdhomContext();
+        public IActionResult Index()
+        {
+            var o_k = db.Products.Include(x=>x.Cat).Select(
+                x => new
+                {
+                    x.Id,  
+                    x.Name,
+                    x.Qty,
+                    x.Price,
+                    catname=x.Cat==null?"":x.Name
+                    
+                }
+                
+                ).ToList();
+            return Ok(o_k);
+        }
+        [HttpGet("GetProducts")]
+        public IActionResult GetProducts()
+        {
+            var o_k = db.Products.ToList();
+            return Ok(o_k);
+        }
+    }
+}
---
+++ ADHOM Store/Controllers/testController.cs
@@ -0,0 +1,14 @@
+﻿using ADHOM_Store.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ADHOM_Store.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
